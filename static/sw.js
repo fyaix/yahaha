89e5b98500cd5ca17b23871d754c1b246bd13ac4
fyaix <121254283+fyaix@users.noreply.github.com>
2025-07-21 20:51:11 +0700

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'vortexvpn-v1';
+const urlsToCache = [
+    '/',
+    '/static/css/style.css',
+    '/static/js/app.js',
+    '/static/manifest.json',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            }
+        )
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
